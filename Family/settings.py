60cfd588f9956c27b0d1aeb46d0c3c556e9f98bf
unknown <sosukegani.tmK@gmail.com>
2018-05-27 00:45:32 +0530

    Shirin

--- Family/settings.py
+++ Family/settings.py
@@ -131,19 +131,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-
-in_heroku = False
-if 'DATABASE_URL' in os.environ:
-    in_heroku = True
-
-import dj_database_url
-
-if in_heroku:
-    DATABASES = {'default': dj_database_url.config()}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
